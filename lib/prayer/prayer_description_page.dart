ffc2b2fd7e981e86cd68d5ac8ac6dde35ba2aaf0
nvi9 <42123156+nvi9@users.noreply.github.com>
2025-02-20 23:39:07 +0100

    show group title before prayer title on description page in case of wider screens

--- lib/prayer/prayer_description_page.dart
+++ lib/prayer/prayer_description_page.dart
@@ -16,54 +16,73 @@ class PrayerDescriptionPage extends StatelessWidget {
   final Prayer prayer;
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar.large(
-              expandedHeight: MediaQuery.of(context).size.height * 0.3,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text(prayer.title),
-                background: PrayerImage(
-                  name: prayer.image,
-                  opacity: const AlwaysStoppedAnimation(.3),
-                ),
-                collapseMode: CollapseMode.parallax,
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            expandedHeight: screenSize.height * 0.3,
+            flexibleSpace: FlexibleSpaceBar(
+              title: screenSize.width > 600
+                  ? Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(text: group.title),
+                          TextSpan(
+                            text: ' / ',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.outline,
+                            ),
+                          ),
+                          TextSpan(text: prayer.title),
+                        ],
+                      ),
+                    )
+                  : Text(prayer.title),
+              background: PrayerImage(
+                name: prayer.image,
+                opacity: const AlwaysStoppedAnimation(.3),
               ),
+              collapseMode: CollapseMode.parallax,
             ),
-            SliverToBoxAdapter(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.fromLTRB(
-                  16,
-                  32,
-                  16,
-                  kMinInteractiveDimension * 2,
-                ),
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints.loose(
-                      const Size.fromWidth(600),
-                    ),
-                    child: Text(
-                      prayer.description,
-                      style: const TextStyle(fontSize: 24),
-                      textAlign: TextAlign.center,
-                    ),
+          ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.fromLTRB(
+                16,
+                32,
+                16,
+                kMinInteractiveDimension * 2,
+              ),
+              child: Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints.loose(
+                    const Size.fromWidth(600),
+                  ),
+                  child: Text(
+                    prayer.description,
+                    style: const TextStyle(fontSize: 24),
+                    textAlign: TextAlign.center,
                   ),
                 ),
               ),
             ),
-          ],
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => PrayerSettingsPage(prayer: prayer),
-            ),
           ),
-          tooltip: 'Ima beállítása',
-          child: const Icon(Icons.check_rounded),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PrayerSettingsPage(prayer: prayer),
+          ),
         ),
-      );
+        tooltip: 'Ima beállítása',
+        child: const Icon(Icons.check_rounded),
+      ),
+    );
+  }
 }
