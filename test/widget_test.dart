230dcc5e868c8f724d4d9d06d8001ff939135525
nvi9 <42123156+nvi9@users.noreply.github.com>
2025-03-01 09:45:35 +0100

    update project name, change app ID and name on other platforms too

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ignacio_prayers_flutter_application/main.dart';
+import 'package:ignaci_imak/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
